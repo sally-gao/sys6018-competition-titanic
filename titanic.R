43912f86e23d65af963a82ba68b988431e92c068
sally-gao <sg2zv@virginia.edu>
2017-08-23 12:29:14 -0400

    update

--- titanic.R
+++ titanic.R
@@ -3,3 +3,25 @@ library(tidyverse)
 test <- read_csv("test.csv")
 train <- read_csv("train.csv")
 
+train$Pclass <- factor(train$Pclass)
+train$Sex <- factor(train$Sex)
+
+train.split <- sample(1:891, size=445)
+train.train <- train[train.split, ]
+train.valid <- train[-train.split, ]
+
+# Just age and sex
+train.model <- glm(Survived~Sex+Age, data=train.train, family = "binomial")
+
+predict1 <-as.vector(predict(train.model, train.valid, type="response"))
+probs1 <- rep(0,446)  # Initialize prediction vector
+probs1[predict1 >0.5] <- 1 # p>0.5 -> 1
+table(probs1, train.valid$Survived) # 77.8%
+
+# Age + sex + parch
+train.model2 <- glm(Survived~Sex+Age+Parch, data=train.train, family = "binomial")
+
+predict2 <-as.vector(predict(train.model2, train.valid, type="response"))
+probs2 <- rep(0,446)  # Initialize prediction vector
+probs2[predict2 >0.5] <- 1 # p>0.5 -> 1
+table(probs2, train.valid$Survived) # 77.8%
